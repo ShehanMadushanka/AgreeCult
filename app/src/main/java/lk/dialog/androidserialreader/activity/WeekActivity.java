31e2e3a29d3abff5e58b7283693cbc4c440484f9
Shehan Madushanka <splintercell92sf@gmail.com>
2017-12-27 15:45:07 +0530

    Initial Commit

---
+++ app/src/main/java/lk/dialog/androidserialreader/activity/WeekActivity.java
@@ -0,0 +1,15 @@
+package lk.dialog.androidserialreader.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lk.dialog.androidserialreader.R;
+
+public class WeekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week);
+    }
+}
